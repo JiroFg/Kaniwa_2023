d442803345442966c3603076732387006ab9a2aa
JiroFg <mechcromancer@gmail.com>
2023-08-24 18:29:17 -0600

    Se agrego el mapa al fragmento

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("me.relex:circleindicator:2.1.6")
+    implementation("com.google.android.gms:play-services-maps:18.1.0")
 }
--- app/src/main/java/com/example/kaniwa_2023/MapFragment.kt
+++ app/src/main/java/com/example/kaniwa_2023/MapFragment.kt
@@ -5,14 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.kaniwa_2023.databinding.FragmentMapBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(R.layout.fragment_map), OnMapReadyCallback {
+
+    private var _binding: FragmentMapBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var map:GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        _binding = FragmentMapBinding.inflate(inflater, container, false)
+        binding.mapView.onCreate(savedInstanceState)
+        binding.mapView.onResume()
+        binding.mapView.getMapAsync(this)
+        return binding.root
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        this.map = map
     }
 }
