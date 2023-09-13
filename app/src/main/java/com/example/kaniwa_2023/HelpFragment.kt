b18741992eed3df000f726726bf0b5224185874a
AlanAHD <alanarmando997@gmail.com>
2023-09-13 10:28:50 -0600

    Correo

--- app/src/main/java/com/example/kaniwa_2023/HelpFragment.kt
+++ app/src/main/java/com/example/kaniwa_2023/HelpFragment.kt
@@ -1,10 +1,12 @@
 package com.example.kaniwa_2023
 
 import android.os.Bundle
+import android.text.util.Linkify
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 class HelpFragment : Fragment() {
 
@@ -13,6 +15,18 @@ class HelpFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_help, container, false)
+        val view= inflater.inflate(R.layout.fragment_help, container, false)
+
+        // Se Obtiene una referencia al TextView
+        val emailTextView: TextView = view.findViewById(R.id.correo)
+
+        // Se Establece el correo electrónico como texto en el TextView
+        val emailAddress = "alanarmando997@gmail.com"
+        emailTextView.text = "Correo electrónico: $emailAddress"
+
+        // Se convierte el texto en un enlace de correo electrónico
+        Linkify.addLinks(emailTextView, Linkify.EMAIL_ADDRESSES)
+
+        return view
     }
 }
