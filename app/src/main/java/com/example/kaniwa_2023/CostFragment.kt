a5d601ddf58497a92ddf0e58677a24cb85472d1b
MarcosDCP <zs20018147@estudiantes.uv.mx>
2023-09-13 10:51:03 -0600

    njdvskjfsnjsjnhdshj

--- app/src/main/java/com/example/kaniwa_2023/CostFragment.kt
+++ app/src/main/java/com/example/kaniwa_2023/CostFragment.kt
@@ -5,14 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
 
 class CostFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
+
+        // Inflar el diseño del fragmento
+        val view = inflater.inflate(R.layout.fragment_cost, container, false)
+
+        // Obtener una referencia al TextView en el diseño
+        val textView = view.findViewById<TextView>(R.id.estudiantesPrecio)
+        val textView2 = view.findViewById<TextView>(R.id.generalPrecio)
+        val textView3 = view.findViewById<TextView>(R.id.inapamPrecio)
+
+        // Cargar la fuente personalizada desde recursos
+        val customFont = ResourcesCompat.getFont(requireContext(), R.font.dinnpro)
+
+        // Aplicar la fuente personalizada al TextView
+        customFont?.let {
+            textView.typeface = it
+            textView2.typeface = it
+            textView3.typeface = it
+        }
+        return view
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cost, container, false)
+
     }
 }
--- app/src/main/java/com/example/kaniwa_2023/HelpFragment.kt
+++ app/src/main/java/com/example/kaniwa_2023/HelpFragment.kt
@@ -7,6 +7,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.content.Intent
+import android.net.Uri
+import android.widget.Button
 
 class HelpFragment : Fragment() {
 
@@ -19,13 +22,34 @@ class HelpFragment : Fragment() {
 
         // Se Obtiene una referencia al TextView
         val emailTextView: TextView = view.findViewById(R.id.correo)
+        val emailButton : Button = view.findViewById(R.id.emailButton)
+        val recipientEmail = "alanarmando997@gmail.com"
+        val subject = "Ayuda referente a Kaniwá"
+        val message = "Detalla tu mensaje aquí..."
 
-        // Se Establece el correo electrónico como texto en el TextView
-        val emailAddress = "alanarmando997@gmail.com"
-        emailTextView.text = "Correo electrónico: $emailAddress"
 
-        // Se convierte el texto en un enlace de correo electrónico
-        Linkify.addLinks(emailTextView, Linkify.EMAIL_ADDRESSES)
+//        // Se Establece el correo electrónico como texto en el TextView
+//        val emailAddress = "alanarmando997@gmail.com"
+//        emailTextView.text = "Correo electrónico: $emailAddress"
+//
+//        // Se convierte el texto en un enlace de correo electrónico
+//        Linkify.addLinks(emailTextView, Linkify.EMAIL_ADDRESSES)
+
+        emailButton.setOnClickListener{
+            val intent = Intent(Intent.ACTION_SEND).apply {
+                data = Uri.parse("mailto:$recipientEmail") // Especifica que es una acción de correo electrónico
+                putExtra(Intent.EXTRA_SUBJECT, subject) // Asunto del correo
+                putExtra(Intent.EXTRA_TEXT, message) // Cuerpo del correo
+            }
+
+            if (intent.resolveActivity(requireActivity().packageManager) != null) {
+                startActivity(intent)
+            } else {
+                // Maneja el caso en el que no haya aplicaciones de correo electrónico disponibles
+                // o maneja el error de otra manera, como mostrar un mensaje de error al usuario.
+            }
+        }
+
 
         return view
     }
